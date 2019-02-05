575bcd290642722e997f603b87e0f19cbb29ce10
Resethel <resethel@gmail.com>
2019-02-05 02:15:06 +0100

    Start implementation of Trails
    This commit starts the implementation of trails. They will be used to trace orbits.
    
    + Added a graphical effect class: Trail

---
+++ src/GFX/Trail.hpp
@@ -0,0 +1,57 @@
+//
+// Trail.hpp
+// N-Body-Simulator
+//
+
+#ifndef TRAIL_HPP
+#define TRAIL_HPP
+
+#include <memory>
+#include <deque>
+
+#include <SFML/Graphics.hpp>
+
+#include "Effect.hpp"
+
+namespace gfx
+{
+
+
+
+    class Trail : public Effect
+    {
+    public:
+
+        typedef std::unique_ptr<Trail> Ptr;
+
+    public:
+
+    	Trail();
+
+        // Methods
+        void    pushNewPoint(const sf::Vector2f& point);
+
+        // Setters
+        void    setMaxPoints(const unsigned& pts);
+
+
+
+    private:
+
+        virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const;
+
+    private:
+
+        size_t                      mMaxPoints;
+        float                       mThickness;
+
+        std::deque<sf::Vector2f>    mPoints;
+        sf::VertexArray             mVertices;
+
+    };
+
+
+
+}
+
+#endif // TRAIL_HPP
